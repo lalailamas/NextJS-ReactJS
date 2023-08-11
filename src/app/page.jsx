d6133a6cd67e412675ebd4b66e45b25d5dd73622
lalailamas <ale.cordoba.p@gmail.com>
2023-08-10 21:49:19 -0400

    Add error handling

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,38 +1,40 @@
-"use client"
+"use client";
 import Table from "../../components/Table";
 import Modal from "../../components/Modal";
-import  { useEffect, useState } from 'react';
+import { useEffect, useState } from "react";
 import getHttp from "./api/getHttp";
 
 function HomePage() {
   const [objects, setObjects] = useState([]);
   const [isModalOpen, setIsModalOpen] = useState(false);
   const [selectedObject, setSelectedObject] = useState(null);
-  
+
   const openModal = (object) => {
-    console.log('estoy en open modal')
     setSelectedObject(object);
     setIsModalOpen(true);
   };
 
   const closeModal = () => {
-    console.log('estoy aqui')
     setSelectedObject(null);
     setIsModalOpen(false);
   };
-  
+
   useEffect(() => {
     async function fetchData() {
-      const objectsData = await getHttp();
-      setObjects(objectsData);
+      try {
+        const objectsData = await getHttp();
+        setObjects(objectsData);
+      } catch (error) {
+        console.error("Error fetching data:", error);
+      }
     }
     fetchData();
   }, []);
 
   return (
     <div className="p-4">
-      <Table objects={objects} openModal={openModal}/>
-      {isModalOpen && <Modal object={selectedObject} closeModal={closeModal}/>}
+      <Table objects={objects} openModal={openModal} />
+      {isModalOpen && <Modal object={selectedObject} closeModal={closeModal} />}
     </div>
   );
 }
