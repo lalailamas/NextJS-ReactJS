035994efcb03068deb9f0d0f88b5f88329272c78
lalailamas <ale.cordoba.p@gmail.com>
2023-08-10 23:01:38 -0400

    OK responsive

--- components/Table.jsx
+++ components/Table.jsx
@@ -4,7 +4,6 @@ function Table({ objects, openModal }) {
   return (
     
     <div className="-my-2 py-2 overflow-x-auto sm:-mx-6 sm:px-6 lg:-mx-8 pr-10 lg:px-8">
-      <section className="align-middle inline-block min-w-full shadow overflow-hidden bg-white shadow-dashboard px-8 pt-3 rounded-bl-lg rounded-br-lg" />
       <table className="min-w-full border border-collapse ">
         <thead>
           <tr>
